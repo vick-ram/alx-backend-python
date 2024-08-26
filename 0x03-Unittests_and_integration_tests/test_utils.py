de0a2b65f73cde843d671b389c401493245716b0
vick-ram <vickramodero6@gmail.com>
2024-08-26 01:45:50 -0700

    commit-3 correction

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -70,7 +70,8 @@ class TestMemoize(unittest.TestCase):
 
         test_instance = TestClass()
 
-        with patch.object(test_instance, 'a_method', return_value=42) as mock_method:
+        with patch.object(test_instance, 'a_method',
+                          return_value=42) as mock_method:
             result1 = test_instance.a_property
             result2 = test_instance.a_property
 
