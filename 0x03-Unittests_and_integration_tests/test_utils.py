7cf352c9a849300ec15e30112c6665306235836e
vick-ram <vickramodero6@gmail.com>
2024-08-26 00:53:45 -0700

    correction

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,7 +20,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",), "a"),
         ({"a": 1}, ("a", "b"), "b"),
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected):
+    def test_access_nested_map_exception(self, nested_map, path, expected_key):
         """test for exception"""
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
